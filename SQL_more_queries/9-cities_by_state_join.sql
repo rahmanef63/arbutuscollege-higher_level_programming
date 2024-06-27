954116468a7847dd2a6810b14914fe17b960a4df
rahmanef63 <rahmanef63@gmail.com>
2024-06-27 14:17:42 +0000

    a

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- using join to display info about two tables
+-- statement to do join query
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
